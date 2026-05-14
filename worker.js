ad73bc047409881fef65d5c9686e07d2a6e45612
Michael Giorgi <94036650+Yorgi11@users.noreply.github.com>
2026-05-14 15:12:54 -0400

    Added worker

---
+++ worker.js
@@ -0,0 +1,33 @@
+import { onRequestPost as createCheckoutSession } from "./functions/create-checkout-session.js";
+import { onRequestPost as createPayPalOrder } from "./functions/create-paypal-order.js";
+import { onRequestPost as verifyOnlinePayment } from "./functions/verify-online-payment.js";
+
+const routes = {
+  "/create-checkout-session": createCheckoutSession,
+  "/create-paypal-order": createPayPalOrder,
+  "/verify-online-payment": verifyOnlinePayment,
+};
+
+function methodNotAllowed() {
+  return new Response(JSON.stringify({ error: "Method not allowed." }), {
+    status: 405,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    const handler = routes[url.pathname];
+
+    if (handler) {
+      if (request.method !== "POST") {
+        return methodNotAllowed();
+      }
+
+      return handler({ request, env, ctx });
+    }
+
+    return env.ASSETS.fetch(request);
+  },
+};
